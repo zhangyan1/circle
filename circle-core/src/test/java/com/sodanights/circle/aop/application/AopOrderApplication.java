276068a651bd71eae640fb68f0107d1d867bb897
zhangyan <578033593@qq.com>
2019-04-02 10:33:51 +0800

    fix

--- circle-core/src/test/java/com/sodanights/circle/aop/application/AopOrderApplication.java
+++ circle-core/src/test/java/com/sodanights/circle/aop/application/AopOrderApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-
-
+import org.springframework.test.context.TestPropertySource;
 
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,DataSourceTransactionManagerAutoConfiguration.class})
