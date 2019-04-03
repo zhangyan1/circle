82448991f5546dd113576a0918114cb5df04072e
zhangyan <578033593@qq.com>
2019-04-03 21:41:17 +0800

    fix

---
+++ circle-core/src/main/java/com/sodanights/circle/core/act/service/impl/SodaAspect.java
@@ -0,0 +1,20 @@
+package com.sodanights.circle.core.act.service.impl;
+
+
+import com.sodanights.circle.client.aop.annotation.Apt;
+import com.sodanights.circle.client.aop.annotation.Mpt;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+@Apt
+public class SodaAspect implements MethodInterceptor {
+
+    @Override
+    @Mpt
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        System.out.println("环绕前");
+        Object object = invocation.proceed();
+        System.out.println("环绕后");
+        return object;
+    }
+}
